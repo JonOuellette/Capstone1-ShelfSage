b021e9f133b9db910f82931961dd0e336045d271
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-10-13 20:58:26 -0700

    Chore: updated models.py to include the database relationships between the tables.  Added additional columns to the books table.

--- models.py
+++ models.py
@@ -11,14 +11,26 @@ class User(db.Model):
     first_name = db.Column(db.String(25), nullable=False)
     last_name = db.Column(db.String(25), nullable=False)
 
+    bookshelves = db.relationship('Bookshelf', backref='user', lazy=True)
+
+    library = db.relationship('Book', secondary='user_library', backref='users', lazy=True)
+
+
 class Book(db.Model):
     __tablename__ = "books"
 
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String, nullable=False)
+    subtitle = db.Column(db.String)
     authors = db.Column(db.String, nullable=False)
     publisher = db.Column(db.String)
     publisher_date = db.Column(db.Date)
+    categories = db.Column(db.Text)
+    image_links = db.Column(db.String)
+    info_link = db.Column(db.String)
+
+    # Define a relationship with users (library)
+    users = db.relationship('User', secondary='user_library', backref='library', lazy=True)
 
 class BookShelf(db.Model):
     __tablename__ = "bookshelves"
@@ -26,6 +38,12 @@ class BookShelf(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.Text)
+
+     # Define foreign key to link to user
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    # Define a relationship with books (shelf content)
+    books = db.relationship('Book', secondary='bookshelf_content', backref='bookshelves', lazy=True)
 
 class UserLibrary(db.Model):
     __tablename__ = "user_library"
